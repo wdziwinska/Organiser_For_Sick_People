4169b1a470f8c7071ce2f8b2e0402567f946ada4
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2022-01-15 00:43:47 +0100

    New Schedule

--- Schedule.py
+++ Schedule.py
@@ -1,26 +1,16 @@
-import schedule
-import datetime
 import time
-from datetime import datetime, timedelta
+import schedule
 
-class Schedule:
 
-    # nowDateTime = datetime.now()
-    # print(nowDateTime)
-    # print(type(nowDateTime))
-    #
-    # nowTime = time.time()
-    # print(nowTime)
-    #
-    # current_time = nowDateTime.strftime("%H:%M")
-    # print("Current Time =", current_time)
-    # schedule.every().day.at(current_time)
+class MySchedule:
 
-    def setTime(self):
-        current_date_and_time = datetime.now()
-        print(current_date_and_time)
+    def setReminder(self, time, action):
+        schedule.every().day.at(time).do(action)
 
-        seconds = 15
-        seconds_added = timedelta(seconds=seconds)
-        future_date_and_time = current_date_and_time + seconds_added
-        print(future_date_and_time)
+
+# mySchedule = MySchedule()
+# mySchedule.setReminder("00:42", lambda: print("dupa"))
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
