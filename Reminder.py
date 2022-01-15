8e8d508a562a3a3078bb810886f76b14a0fb9847
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2022-01-15 01:58:27 +0100

    Schedule.py renamed to Reminder.py

---
+++ Reminder.py
@@ -0,0 +1,27 @@
+import time
+import schedule
+
+
+class Reminder:
+
+    _active = True
+
+    def __init__(self, time, action):
+        def callback():
+            if self._active:
+                action()
+
+        schedule.every().day.at(time).do(callback)
+
+    def activate(self):
+        self._active = True
+
+    def deactivate(self):
+        self._active = False
+
+reminder = Reminder("01:54", lambda: print("test"))
+# mySchedule.deactivate()
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
