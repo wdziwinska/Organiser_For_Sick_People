dc48a1e12bfc660fc39577cc0dc559739f469364
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2022-01-18 21:00:22 +0100

    Added class "AddPulse" (adding new pulse value)

--- MyGUI.py
+++ MyGUI.py
@@ -1,7 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.theming import ThemeManager
 from kivymd.uix.picker import MDDatePicker, MDTimePicker
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
@@ -9,56 +8,15 @@ from kivy.core.window import Window
 from Notifier import Notifier
 from Reminder import Reminder
 
-from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
-from kivymd.app import MDApp
 from matplotlib import pyplot as plt
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
-x = [10, 11, 12, 13, 14, 15]
-y = [98, 87, 67, 78, 90, 103]
-
-plt.plot(x, y, color='r', linestyle='--', marker='.')
-plt.xlabel("Date")
-plt.ylabel("Pulse")
+import csv
 
 Window.size = (750, 450)
 
-messageTitle = "Przypomnienie o lekach"
-message = "Weź leki!"
-
-class PlotWindow(Screen):
-    def plotFunction(self):
-
-        box = self.ids.box
-        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-
-    def savePlot(self):
-        name = self.ids.namer.text
-        if name:
-            plt.savefig(name)
-
-
-class SecondWindow(Screen):
-    def pressButtonZatwierdz(self):
-        myGUI = MyGUI()
-        myGUI.show_date_picker()
-
-        messageTitle = self.ids.msgTitle.text
-        message = self.ids.msg.text
-        print("tit nad mess: ", messageTitle, message)
-
-
-class MainWindow(Screen):
-    pass
-
-
-class ListWindow(Screen):
-    pass
-
-
-class WindowManager(ScreenManager):
-    pass
+messageTitle = "None"
+message = "None"
 
 
 class MyGUI(MDApp):
@@ -110,3 +68,55 @@ class MyGUI(MDApp):
         pressed.text = f"You pressed {list_id}"
 
 
+class PlotWindow(Screen):
+    def plotFunction(self):
+        x = [10, 11, 12, 13, 14, 15]
+        y = [98, 87, 67, 78, 90, 103]
+
+        plt.plot(x, y, color='r', linestyle='--', marker='.')
+        plt.xlabel("Date")
+        plt.ylabel("Pulse")
+
+        box = self.ids.box
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+
+
+class SecondWindow(Screen):
+    def pressButtonZatwierdz(self):
+        myGUI = MyGUI()
+        myGUI.show_date_picker()
+
+        messageTitle = self.ids.msgTitle.text
+        message = self.ids.msg.text
+        print("tit nad mess: ", messageTitle, message)
+
+
+class MainWindow(Screen):
+    pass
+
+
+class ListWindow(Screen):
+    pass
+
+
+class AddPulse(Screen):
+    def savePlot(self):
+        # name = self.ids.namer.text
+        # if name:
+        #     plt.savefig(name)
+
+        pulseValue = self.ids.pulseValue.text
+        print(pulseValue)
+
+        with open('PulseDate.csv', 'w', newline='') as csvFile:
+            writer = csv.writer(csvFile, quoting=csv.QUOTE_NONNUMERIC, delimiter=';')
+            writer.writerow(pulseValue)
+
+        # csvFile = open("PulseDate.csv", 'w', newline=' ')
+        # writer = csv.writer(csvFile)
+        # writer.writerow('65')
+        # csvFile.close()
+
+
+class WindowManager(ScreenManager):
+    pass
