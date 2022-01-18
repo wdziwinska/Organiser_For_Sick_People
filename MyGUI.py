8e6d141616df514078caa726f481815b240ccdb2
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2022-01-18 18:48:14 +0100

    Loading messageTitle and message from kivy's TextInput (but not for 'reminder', only for variable)

--- MyGUI.py
+++ MyGUI.py
@@ -24,9 +24,44 @@ plt.ylabel("Pulse")
 
 Window.size = (750, 450)
 
+messageTitle = "Przypomnienie o lekach"
+message = "Weź leki!"
+
+class PlotWindow(Screen):
+    def plotFunction(self):
+
+        box = self.ids.box
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+
+    def savePlot(self):
+        name = self.ids.namer.text
+        if name:
+            plt.savefig(name)
+
+
+class SecondWindow(Screen):
+    def pressButtonZatwierdz(self):
+        myGUI = MyGUI()
+        myGUI.show_date_picker()
+
+        messageTitle = self.ids.msgTitle.text
+        message = self.ids.msg.text
+        print("tit nad mess: ", messageTitle, message)
+
+
+class MainWindow(Screen):
+    pass
+
+
+class ListWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
 
 class MyGUI(MDApp):
-
     notifier = Notifier()
 
     def build(self):
@@ -34,11 +69,12 @@ class MyGUI(MDApp):
         self.theme_cls.primary_palette = "Blue"
         return Builder.load_file("AppDesign.kv")
 
-    def callback(self):
-        self.notifier.notify("Przypomnienie o lekach", "Wez leki")
+    def call(self):
+        print("messageTitle and message: ", messageTitle, message)
+        self.notifier.notify(messageTitle, message)
 
     def save_time(self, instance, time):
-        reminder = Reminder(str(time), self.callback)
+        reminder = Reminder(str(time), self.call)
         print(str(time))
 
     def cancel_time(self, instance, time):
@@ -52,6 +88,7 @@ class MyGUI(MDApp):
         time_dialog.bind(on_save=self.save_time, on_cancel=self.cancel_time)
         time_dialog.open()
 
+    # dodawanie przypomnienia w przedziale czasowym!!
     def save_date(self, instance, value, date_range):
         # print(instance, value, date_range)
         print(str(value))
@@ -73,29 +110,3 @@ class MyGUI(MDApp):
         pressed.text = f"You pressed {list_id}"
 
 
-class PlotWindow(Screen):
-    def plotFunction(self):
-        box = self.ids.box
-        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-
-    def savePlot(self):
-        name = self.ids.namer.text
-        if name:
-            plt.savefig(name)
-
-
-class SecondWindow(Screen):
-    def pressButtonZatwierdz(self):
-        myGUI = MyGUI()
-        myGUI.show_date_picker()
-
-
-class MainWindow(Screen):
-    pass
-
-class ListWindow(Screen):
-    pass
-
-
-class WindowManager(ScreenManager):
-    pass
